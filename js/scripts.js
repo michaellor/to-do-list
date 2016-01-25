023a7f128ca749af0d6a4cfb68e838ff96aa2aef
Erik Tolentino and Michael Lor <tolentino3575@gmail.com>
2016-01-25 15:28:11 -0800

    add html specifications

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,47 @@
-function Place(placeName, placeLocation, placeLandmark, placeTime) {
-  this.placeName = placeName;
-  this.placeLocation = placeLocation;
-  this.placeLandmark = placeLandmark;
-  this.placeTime = placeTime;
+function Tasks(task1, task2, task3, task4) {
+  this.task1 = task1;
+  this.task2 = task2;
+  this.task3 = task3;
+  this.task4 = task4;
 };
 
 
-Place.prototype.fullLocation = function() {
-  return this.placeName + ", " + this.placeLocation;
+Tasks.prototype.removeTask = function() {
+  return this.task1 = "";
 };
 
-Place.prototype.fullDescription = function() {
-  return this.placeLandmark + " " + this.placeTime;
-};
-
-$(document).ready(function(){
-  $("form#masterPlace").submit(function(event){
-
-    var inputName = $("input#placeName").val();
-    var inputLocation = $("input#placeLocation").val();
-    var inputLandmark = $("input#placeLandmark").val();
-    var inputTime = $("input#placeTime").val();
-    var newPlace = new Place(inputName, inputLocation, inputLandmark, inputTime);
-
-    $("ul#info").append("<p><span class='placename'>" + newPlace.fullLocation() + "</span></p>");
-
-    $("input#placeName").val("");
-    $("input#placeLocation").val("");
-    $("input#placeLandmark").val("");
-    $("input#placeTime").val("");
-
-    event.preventDefault();
-
-    $(".placename").last().click(function() {
-      $("#show-info").show();
-      $("#show-info h2").text(newPlace.fullLocation());
-      $(".place-name").text(newPlace.fullLocation());
-      $(".place-location").text(newPlace.placeLocation);
-      $(".place-landmark").text(newPlace.placeLandmark);
-      $(".place-time").text(newPlace.placeTime);
-    })
-
-
-  });
-});
+//
+// Place.prototype.fullDescription = function() {
+//   return this.placeLandmark + " " + this.placeTime;
+// };
+//
+// $(document).ready(function(){
+//   $("form#masterPlace").submit(function(event){
+//
+//     var inputName = $("input#placeName").val();
+//     var inputLocation = $("input#placeLocation").val();
+//     var inputLandmark = $("input#placeLandmark").val();
+//     var inputTime = $("input#placeTime").val();
+//     var newPlace = new Place(inputName, inputLocation, inputLandmark, inputTime);
+//
+//     $("ul#info").append("<p><span class='placename'>" + newPlace.fullLocation() + "</span></p>");
+//
+//     $("input#placeName").val("");
+//     $("input#placeLocation").val("");
+//     $("input#placeLandmark").val("");
+//     $("input#placeTime").val("");
+//
+//     event.preventDefault();
+//
+//     $(".placename").last().click(function() {
+//       $("#show-info").show();
+//       $("#show-info h2").text(newPlace.fullLocation());
+//       $(".place-name").text(newPlace.fullLocation());
+//       $(".place-location").text(newPlace.placeLocation);
+//       $(".place-landmark").text(newPlace.placeLandmark);
+//       $(".place-time").text(newPlace.placeTime);
+//     })
+//
+//
+//   });
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,22 +1,28 @@
-describe('Place', function() {
-  it("creates a new place with the given properties.", function(){
-  var testPlace = new Place("Maui","Hawaii","Beach", "Summer");
-  expect(testPlace.placeName).to.equal("Maui")
-  expect(testPlace.placeLocation).to.equal("Hawaii");
-  expect(testPlace.placeLandmark).to.equal("Beach");
-  expect(testPlace.placeTime).to.equal("Summer");
+describe('Tasks', function() {
+  it("creates a new list with the given properties.", function(){
+  var testList = new Tasks("Laundry","Vacuum","Groceries", "Cook");
+  expect(testList.task1).to.equal("Laundry")
+  expect(testList.task2).to.equal("Vacuum");
+  expect(testList.task3).to.equal("Groceries");
+  expect(testList.task4).to.equal("Cook");
   });
 
+  it("removes a task from the list.", function() {
+  var testList = new Tasks("Laundry","Vacuum","Groceries", "Cook");
+  expect(testList.removeTask()).to.equal("");
 
-  it("adds fullDescription method to a place.", function() {
-    var testPlace = new Place ("Maui","Hawaii","Beach", "Summer");
-    expect(testPlace.fullDescription()).to.equal("Beach Summer");
   });
 
-  it("adds fullName method to a place.", function() {
-    var testPlace = new Place("Maui","Hawaii","Beach", "Summer");
-    expect(testPlace.fullLocation()).to.equal("Maui Hawaii");
-  });
-
+  //
+  // it("adds fullDescription method to a place.", function() {
+  //   var testPlace = new Place ("Maui","Hawaii","Beach", "Summer");
+  //   expect(testPlace.fullDescription()).to.equal("Beach Summer");
+  // });
+  //
+  // it("adds fullName method to a place.", function() {
+  //   var testPlace = new Place("Maui","Hawaii","Beach", "Summer");
+  //   expect(testPlace.fullLocation()).to.equal("Maui Hawaii");
+  // });
+  //
 
 });
