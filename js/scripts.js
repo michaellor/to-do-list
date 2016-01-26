ee1879e2605fe804e8ecd3038067d0c80c0987bb
Erik Tolentino and Michael Lor <tolentino3575@gmail.com>
2016-01-25 16:49:54 -0800

    add prototype for task return

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,32 @@
-function Tasks(task1, task2, task3, task4) {
+function tasks(task1, task2) {
   this.task1 = task1;
-  this.task2 = task2;
-  this.task3 = task3;
-  this.task4 = task4;
+  // this.task2 = task2;
 };
 
+tasks.prototype.ensure = function() {
+  return this.task1;
+}
 
-Tasks.prototype.removeTask = function() {
-  return this.task1 = "";
-};
 
-//
-// Place.prototype.fullDescription = function() {
-//   return this.placeLandmark + " " + this.placeTime;
-// };
-//
-// $(document).ready(function(){
-//   $("form#masterPlace").submit(function(event){
-//
-//     var inputName = $("input#placeName").val();
-//     var inputLocation = $("input#placeLocation").val();
-//     var inputLandmark = $("input#placeLandmark").val();
-//     var inputTime = $("input#placeTime").val();
-//     var newPlace = new Place(inputName, inputLocation, inputLandmark, inputTime);
-//
-//     $("ul#info").append("<p><span class='placename'>" + newPlace.fullLocation() + "</span></p>");
-//
-//     $("input#placeName").val("");
-//     $("input#placeLocation").val("");
-//     $("input#placeLandmark").val("");
-//     $("input#placeTime").val("");
-//
-//     event.preventDefault();
-//
-//     $(".placename").last().click(function() {
-//       $("#show-info").show();
-//       $("#show-info h2").text(newPlace.fullLocation());
-//       $(".place-name").text(newPlace.fullLocation());
-//       $(".place-location").text(newPlace.placeLocation);
-//       $(".place-landmark").text(newPlace.placeLandmark);
-//       $(".place-time").text(newPlace.placeTime);
-//     })
-//
-//
-//   });
-// });
+
+
+$(document).ready(function(){
+  $("form#masterPlace").submit(function(event){
+
+    var inputTask1 = $("input#task1").val();
+    // var inputTask2 = $("input#task2").val();
+    var newTask = new tasks(inputTask1);
+
+    $("ul#info").append('<li>' + newTask.ensure());
+
+    $("input#task1").val("");
+    $("input#task2").val("");
+    //
+    // $("#info").click(function(){
+    //   $("#info").hide();
+    // })
+
+    event.preventDefault();
+
+  });
+});
